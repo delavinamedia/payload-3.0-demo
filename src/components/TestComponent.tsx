05ce95a778a360cabce2380a692c7f6585882cd3
delavinamedia <koeppen.h@gmail.com>
2024-05-07 19:21:47 +0200

    Create TestComponent.tsx

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TestComponent = () => {
+  return <div className="bg-green-500 p-4 text-green-100">Hey, this is a TestComponent!</div>
+}
+
+export default TestComponent
